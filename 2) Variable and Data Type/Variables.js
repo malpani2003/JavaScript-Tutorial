87f6e3a120a6fead3a75ed894d1e084c3f6e2c96
Pranav Malpani <98647622+malpani2003@users.noreply.github.com>
2022-09-27 19:02:23 +0530

    Add files via upload
    Variable , Data Type and typeof( ) operator

---
+++ 2) Variable and Data Type/Variables.js
@@ -0,0 +1,15 @@
+
+// declare a variable use keyword var
+var score;
+console.log(score);
+
+// declare  a variable and initilaize
+var liveleft = 3;  // assignment operartor is used
+console.log(liveleft);
+
+//re-asign value
+liveleft = 2;
+console.log(liveleft);
+
+//constat
+const fistname = "Pranav";
