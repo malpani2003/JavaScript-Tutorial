594ab2bbcc49fa8c83c286a8c4636849999429ec
Pranav Malpani <98647622+malpani2003@users.noreply.github.com>
2022-09-29 20:54:42 +0530

    Add files via upload

---
+++ 4) Conditional Statement/if_else.js
@@ -0,0 +1,26 @@
+////////// if -- else 
+
+let n = 10;
+if (n > 0) {
+    console.log("n is positive");
+}
+else {
+    console.log("n is negative")
+}
+// else part is optional 
+
+
+//////////  if -- else if -- else
+var myage=19;
+if(myage>30){
+    console.log("You are over 30");
+}
+else if(myage>20){
+    console.log("You are over 20");
+}
+else if(myage>10){
+    console.log("You are over 10");
+}
+else{
+    console.log("You are below 10")
+}
---
+++ 4) Conditional Statement/switch.js
@@ -0,0 +1,18 @@
+var x=3;
+switch (x) {
+    case 1:
+        console.log("x is 1");
+        break;
+    case 2:
+        console.log("x is 2");
+        break;
+    case 3:
+        console.log("x is 3");
+        break;
+    case 4:
+        console.log("x is 4");
+        break;
+    default:
+        console.log("Number is not 1,2,3,4")
+        break;
+}
